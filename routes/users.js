83f7636f5b57cdcc670bfeadd068aae664cf0c7e
Vinay Rao <vrao8912@gmail.com>
2022-11-28 10:47:27 +0100

    cleanup file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/users", function (req, res, next) {
+  res.send("Send All User Info");
+});
+
+router.get("/users/:id", function (req, res, next) {
+  res.send("Send user based on id");
 });
 
 module.exports = router;
